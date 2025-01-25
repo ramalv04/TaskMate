dcb4c24c6bc1d90b6c5bdf3bcdafb1d00e2385e1
ramalv04 <ramiro.alvarez.ices@gmail.com>
2025-01-25 00:39:03 -0300

    CSRF

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 DEBUG = env("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "taskmate-alv.up.railway.app"]
-CSRF_TRUSTED_ORIGINS = ["taskmate-alv.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://taskmate-alv.up.railway.app"]
 
 
 # Application definition
